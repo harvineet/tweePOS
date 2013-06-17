c96d2613a4674057257129eb0bdeaf3e6c43d1eb
harvinsi <harvinsi@harvinsi-w7.corp.adobe.com>
2013-06-17 15:16:47 +0530

    function for given tweet added which returns proper nouns

--- tweetNLP/src/tagFetcher.java
+++ tweetNLP/src/tagFetcher.java
@@ -6,8 +6,25 @@ import cmu.arktweetnlp.Tagger.TaggedToken;
 
 
 public class tagFetcher {
+	public static ArrayList<String> tweetPOS(String tweet) throws IOException {
+		String modelFilename = "model.20120919"; //args[0];
+
+		Tagger tagger = new Tagger();
+		tagger.loadModel(modelFilename);
+		ArrayList<String> pos = new ArrayList<String>();
+		ArrayList<TaggedToken> taggedTokens = (ArrayList<TaggedToken>) tagger.tokenizeAndTag(tweet);
+		for (TaggedToken token : taggedTokens) {
+			System.out.printf("%s\t%s\n", token.tag, token.token);
+			if(token.tag.equals("^") || token.tag.equals("Z") ||token.tag.equals("M")) {
+				pos.add(token.tag);
+				//System.out.printf("%s\t%s\n", token.tag, token.token);
+			}
+			
+		}
+		return pos;
+	}
 	public static void main(String[] args) throws IOException {
-		if (args.length < 1) {
+		/*if (args.length < 1) {
 			System.out.println("Supply the model filename as first argument.");
 		}
 		String modelFilename = "model.20120919"; //args[0];
@@ -21,7 +38,9 @@ public class tagFetcher {
 		for (TaggedToken token : taggedTokens) {
 			System.out.printf("%s\t%s\n", token.tag, token.token);
 			
-		}
+		}*/
+		String tweet= "Sen McCain says no decision by President Obama on arming rebels, decision is that the \"red line\" has been crossed #Syria #CW #Assad";
+		tweetPOS(tweet);
 	}
 
 }
